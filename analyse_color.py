b50ad6031d3bc24354f6176e6d27bdafcf2294b4
Sophie 'Tyalie' Friedrich <dev@flowerpot.me>
2023-09-09 00:49:36 +0200

    verifier: Move verify code into own importable file
    Previously all verification logic happened in the tool itself, which
    was impractical as I also will require it in the modify script.
    
    Signed-off-by: Sophie 'Tyalie' Friedrich <dev@flowerpot.me>

--- analyse_color.py
+++ analyse_color.py
@@ -1,79 +1,12 @@
 #!/usr/bin/env python3
 import argparse
 from analyze_lib import *
-import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="Input file to analyze", required=True)
-parser.add_argument("-e", "--extract", help="Output folder to extract to", required=False)
 args = parser.parse_args()
 
 with open(args.file, "rb") as fp:
   data = fp.read()
 
-# check magic bytes
-assert data[:4] == b"\x90\x80\x30\x6A", "Incompatible magic bytes"
-
-# check type
-assert (c := data[4:8]) == b"\x30\x00\x10\x03", f"Incorrect type (got {c})"
-
-_given_checksum = int.from_bytes(data[0xE:0xE + 2], byteorder="little", signed=False)
-_real_checksum = calc_checksum(data)
-assert _given_checksum == _real_checksum, f"Mismatching checksum expected {_real_checksum:02x}"
-
-# size (bytes) - seesm to have offset of 0x80 (start of table)
-_size = 0x80 + int.from_bytes(data[0x10:0x14], byteorder="little", signed=False)
-assert _size == len(data), f"Incorrect length in header (got {_size:08x})"
-
-# check if color table
-_table_type = read_null_str(data[0x20:])
-assert _table_type == b"FP-COLOR", f"Incorrect table type. Make sure its color (got {_table_type})"
-
-_version_name = read_null_str(data[0x40:])
-assert _version_name is not None
-assert re.match(rb"V\d{4}[A-Z]+_[A-Z]+", _version_name), f"Version didn't match pattern (get {_version_name})"
-_version = int(_version_name[1:5])
-
-# - match end of file with version number
-assert _version == int(data[-3:]), f"Version didn't match. Abort (got {_version} vs {data[-3:]})"
-
-_bitmap_list = list(parse_bitmap_table(data))
-
-# start of bitmap files
-_bitmap_start = _bitmap_list[0]
-
-# - check whether all bitmaps are chained after each other
-def check_bitmap_chain(data, b_offset=_bitmap_start):
-  addr = b_offset
-  _idx = 0
-
-  while True:
-    _header = data[addr:addr + 2]
-
-    if _header != b"BM":
-      print(f"Couldn't find BM bitmap (loc 0x{addr:08x})")
-
-      _i = find_next_block(data[addr:])
-      if _i is None:
-        print("- Rest is empty -> good")
-        return True
-      else:
-        print(f"- Found next bytes at (loc 0x{addr + _i:08x})")
-        return False
-
-    assert addr == _bitmap_list[_idx], f"Location mismatch (got {addr:08x} expected {_bitmap_list[_idx]:08x}@{_idx})"
-
-    _length = int.from_bytes(data[addr + 2:addr + 6], byteorder="little", signed=False)
-
-    if args.extract is not None:
-      with open(f"{args.extract}/{_idx:04}-{addr:08X}.bmp", "wb") as fp:
-        fp.write(data[addr:addr + _length])
-
-    addr += _length
-    _idx += 1
-
-    if addr >= len(data):
-      print("success")
-      return True
-
-check_bitmap_chain(data[:-3])
+verify_file(data)
--- analyze_lib.py
+++ analyze_lib.py
@@ -1,4 +1,11 @@
 import itertools
+import re
+
+MAGIC_BYTES = b"\x90\x80\x30"
+COLOR_MAGIC_BYTES = b"\x30\x00\x01\x03"
+TABLE_START_IDX = 0x80
+STR_COLOR_TABLE = b"FP-COLOR"
+RE_VERSION_PATTERN = rb"V\d{4}[A-Z]+_[A-Z]+"
 
 class VerifyException(Exception):
   ...
@@ -46,16 +53,86 @@ def parse_bitmap_table(data, t_offset=0x80):
     else:
       print(f"- Got undefined location at idx {idx} -> ignore")
 
+def check_bitmap_chain(data, _bitmap_list, b_offset):
+  """
+  Check whether all bmp files are chained together
+  also verify that detected position matches with bitmap list
+  """
+  addr = b_offset
+  _idx = 0
 
-def verify_file(data, device=0x6A) -> bool:
+  while True:
+    _header = data[addr:addr + 2]
+
+    if _header != b"BM":
+      print(f"Couldn't find BM bitmap (loc 0x{addr:08x})")
+
+      _i = find_next_block(data[addr:])
+      if _i is None:
+        print("- Rest is empty -> good")
+        return True
+      else:
+        print(f"- Found next bytes at (loc 0x{addr + _i:08x})")
+        return False
+
+    if addr != _bitmap_list[_idx]:
+      raise VerifyException(f"Location mismatch (got {addr:08x} expected {_bitmap_list[_idx]:08x}@{_idx})")
+
+    _length = int.from_bytes(data[addr + 2:addr + 6], byteorder="little", signed=False)
+
+    addr += _length
+    _idx += 1
+
+    if addr >= len(data):
+      print("success")
+      return True
+
+def verify_header(data, device) -> bool:
   # check magic bytes (for device 0x6A)
-  if data[:3] == b"\x90\x80\x30" and data[3] == device:
-    raise VerifyException("Incompatible magic bytes")
+  if data[:3] != MAGIC_BYTES and data[3] == device:
+    raise VerifyException(f"Incompatible magic bytes (got {data[:3].hex()}; expected {MAGIC_BYTES.hex()})")
+
+  if (c := data[4:8]) == COLOR_MAGIC_BYTES:
+    raise VerifyException(f"Incorrect type (got {c})")
 
   _given_checksum = int.from_bytes(data[0xE:0xE + 2], byteorder="little", signed=False)
   _real_checksum = calc_checksum(data)
-
   if _given_checksum != _real_checksum:
     raise VerifyException(f"Mismatching checksum (excepted {_real_checksum:02x} got {_given_checksum:02x})")
 
-  ...
+  # verify file size
+  _size = TABLE_START_IDX + int.from_bytes(data[0x10:0x14], byteorder="little", signed=False)
+  if _size != len(data):
+    raise VerifyException(f"Incorrect length in header (got {_size:08x})")
+
+  # check if it is a color table
+  _table_type = read_null_str(data[0x20:])
+  if _table_type is None or _table_type != STR_COLOR_TABLE:
+    raise VerifyException(f"Incorrect table type. Make sure its color (got {_table_type})")
+
+  _version_name = read_null_str(data[0x40:])
+  if _version_name is None or not re.match(RE_VERSION_PATTERN, _version_name):
+    raise VerifyException(f"Version didn't match pattern (get {_version_name})")
+
+  # match end of file, which should match version
+  _version = int(_version_name[1:5])
+  if _version != int(data[-3:]):
+    raise VerifyException(f"Version didn't match. Abort (got {_version} vs {data[-3:]})")
+
+  return True
+
+
+def verify_body(data) -> bool:
+  _bitmap_list = list(parse_bitmap_table(data))
+  _bitmap_start = _bitmap_list[0]
+
+  if not check_bitmap_chain(data[:-3], _bitmap_list, _bitmap_start):
+    raise VerifyException("Couldn't verify body")
+
+  return True
+
+
+def verify_file(data, device=0x6A) -> bool:
+  verify_header(data, device)
+  verify_body(data)
+  return True
